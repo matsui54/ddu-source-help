376500ef154203ed48699fc01af990e8a5bca936
matsui54 <matsui54@users.noreply.github.com>
2022-03-26 00:58:34 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddu-source-help/ddu-source-help/denops/@ddu-kinds/help.ts
    [1/2] Looking for releases: https://deno.land/x/ddu_vim@v1.3.0/types.ts
    [1/2] Using latest: https://deno.land/x/ddu_vim@v1.3.0/types.ts
    [2/2] Looking for releases: https://deno.land/x/ddu_vim@v1.3.0/deps.ts
    [2/2] Using latest: https://deno.land/x/ddu_vim@v1.3.0/deps.ts
    
    /home/runner/work/ddu-source-help/ddu-source-help/denops/@ddu-sources/help.ts
    [1/3] Looking for releases: https://deno.land/x/ddu_vim@v1.3.0/types.ts
    [1/3] Using latest: https://deno.land/x/ddu_vim@v1.3.0/types.ts
    [2/3] Looking for releases: https://deno.land/x/ddu_vim@v1.3.0/deps.ts
    [2/3] Using latest: https://deno.land/x/ddu_vim@v1.3.0/deps.ts
    [3/3] Looking for releases: https://deno.land/std@0.131.0/path/mod.ts
    [3/3] Attempting update: https://deno.land/std@0.131.0/path/mod.ts -> 0.132.0
    [3/3] Update successful: https://deno.land/std@0.131.0/path/mod.ts -> 0.132.0
    
    /home/runner/work/ddu-source-help/ddu-source-help/README.md
    
    Already latest version:
    https://deno.land/x/ddu_vim@v1.3.0/types.ts == v1.3.0
    https://deno.land/x/ddu_vim@v1.3.0/deps.ts == v1.3.0
    https://deno.land/x/ddu_vim@v1.3.0/types.ts == v1.3.0
    https://deno.land/x/ddu_vim@v1.3.0/deps.ts == v1.3.0
    
    Successfully updated:
    https://deno.land/std@0.131.0/path/mod.ts 0.131.0 -> 0.132.0
    make[1]: Entering directory '/home/runner/work/ddu-source-help/ddu-source-help'
    make[1]: Leaving directory '/home/runner/work/ddu-source-help/ddu-source-help'

--- denops/@ddu-sources/help.ts
+++ denops/@ddu-sources/help.ts
@@ -1,6 +1,6 @@
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v1.3.0/types.ts";
 import { Denops, op } from "https://deno.land/x/ddu_vim@v1.3.0/deps.ts";
-import { dirname, join } from "https://deno.land/std@0.131.0/path/mod.ts";
+import { dirname, join } from "https://deno.land/std@0.132.0/path/mod.ts";
 import { ActionData } from "../@ddu-kinds/help.ts";
 
 type Params = {
