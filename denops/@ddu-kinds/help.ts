fe44478ad8d46f5811334b57152bc79959701826
matsui54 <matsui54@users.noreply.github.com>
2022-05-28 01:15:22 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddu-source-help/ddu-source-help/denops/@ddu-kinds/help.ts
    [1/2] Looking for releases: https://deno.land/x/ddu_vim@v1.6.0/types.ts
    [1/2] Attempting update: https://deno.land/x/ddu_vim@v1.6.0/types.ts -> v1.7.0
    [1/2] Update successful: https://deno.land/x/ddu_vim@v1.6.0/types.ts -> v1.7.0
    [2/2] Looking for releases: https://deno.land/x/ddu_vim@v1.6.0/deps.ts
    [2/2] Attempting update: https://deno.land/x/ddu_vim@v1.6.0/deps.ts -> v1.7.0
    [2/2] Update successful: https://deno.land/x/ddu_vim@v1.6.0/deps.ts -> v1.7.0
    
    /home/runner/work/ddu-source-help/ddu-source-help/denops/@ddu-sources/help.ts
    [1/3] Looking for releases: https://deno.land/x/ddu_vim@v1.6.0/types.ts
    [1/3] Attempting update: https://deno.land/x/ddu_vim@v1.6.0/types.ts -> v1.7.0
    [1/3] Update successful: https://deno.land/x/ddu_vim@v1.6.0/types.ts -> v1.7.0
    [2/3] Looking for releases: https://deno.land/x/ddu_vim@v1.6.0/deps.ts
    [2/3] Attempting update: https://deno.land/x/ddu_vim@v1.6.0/deps.ts -> v1.7.0
    [2/3] Update successful: https://deno.land/x/ddu_vim@v1.6.0/deps.ts -> v1.7.0
    [3/3] Looking for releases: https://deno.land/std@0.140.0/path/mod.ts
    [3/3] Attempting update: https://deno.land/std@0.140.0/path/mod.ts -> 0.141.0
    [3/3] Update successful: https://deno.land/std@0.140.0/path/mod.ts -> 0.141.0
    
    /home/runner/work/ddu-source-help/ddu-source-help/README.md
    
    Successfully updated:
    https://deno.land/x/ddu_vim@v1.6.0/types.ts v1.6.0 -> v1.7.0
    https://deno.land/x/ddu_vim@v1.6.0/deps.ts v1.6.0 -> v1.7.0
    https://deno.land/x/ddu_vim@v1.6.0/types.ts v1.6.0 -> v1.7.0
    https://deno.land/x/ddu_vim@v1.6.0/deps.ts v1.6.0 -> v1.7.0
    https://deno.land/std@0.140.0/path/mod.ts 0.140.0 -> 0.141.0
    make[1]: Entering directory '/home/runner/work/ddu-source-help/ddu-source-help'
    make[1]: Leaving directory '/home/runner/work/ddu-source-help/ddu-source-help'

--- denops/@ddu-kinds/help.ts
+++ denops/@ddu-kinds/help.ts
@@ -4,8 +4,8 @@ import {
   BaseKind,
   DduItem,
   Previewer,
-} from "https://deno.land/x/ddu_vim@v1.6.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v1.6.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v1.7.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v1.7.0/deps.ts";
 
 export type ActionData = {
   word: string;
