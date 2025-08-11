f0a46f9fea8ca96519c1bc554dd86184eac1a869
matsui54 <haru.matu9168@gmail.com>
2025-08-11 17:43:48 +0900

    :package: bump @std/path from ~1.0.6 to ~1.1.0

--- denops/@ddu-sources/help.ts
+++ denops/@ddu-sources/help.ts
@@ -3,7 +3,7 @@ import { Item } from "jsr:@shougo/ddu-vim@~10.4.0/types";
 import { Denops } from "jsr:@denops/std@~7.6.0";
 import * as fn from "jsr:@denops/std@~7.6.0/function";
 import * as op from "jsr:@denops/std@~7.6.0/option";
-import { dirname, join } from "jsr:@std/path@~1.0.6";
+import { dirname, join } from "jsr:@std/path@~1.1.0";
 import { ActionData } from "../@ddu-kinds/help.ts";
 
 type Params = {
